ea436d0c943aa5b14eaa1c002872b412932e8b45
Marco Caciotti <mcaciotti@gmail.com>
2026-07-11 12:43:16 +0200

    reame 0.1.2 (renamed)

---
+++ Formula/reame.rb
@@ -0,0 +1,41 @@
+class Reame < Formula
+  desc "CPU inference server that gets faster the longer it runs"
+  homepage "https://github.com/swellweb/reame"
+  url "https://github.com/swellweb/reame.git",
+      tag:      "v0.1.2",
+      revision: "7d10b7fbf3a6d8090ead879e49ce8f493a3c1f50"
+  license "MIT"
+  head "https://github.com/swellweb/reame.git", branch: "main"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "boost" => :build
+  depends_on "nlohmann-json" => :build
+  depends_on "zstd"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build",
+           "-DCMAKE_BUILD_TYPE=Release",
+           "-DREAME_BUILD_TESTS=OFF",
+           *std_cmake_args
+    system "cmake", "--build", "build", "--parallel"
+    bin.install "build/src/reame"
+    pkgshare.install "config/reame.conf" => "reame.conf.example"
+  end
+
+  def caveats
+    <<~EOS
+      Zero-config start (downloads a small model on first use):
+        reame run qwen2.5-1.5b
+
+      Or with a config file for full control:
+        cp #{pkgshare}/reame.conf.example ~/reame.conf
+        # edit model.path, then:
+        reame --config ~/reame.conf --serve
+    EOS
+  end
+
+  test do
+    assert_match "reame", shell_output("#{bin}/reame --version")
+  end
+end
